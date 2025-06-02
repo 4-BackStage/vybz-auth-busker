f0ec4c58965a136d36a17e2e69834829e4f687fa
jijihorang <jiho7213@gmail.com>
2025-06-03 00:10:32 +0900

    feat: auth-busker 초기 프로젝트 설정

---
+++ src/main/java/back/vybz/auth_busker/AuthBuskerApplication.java
@@ -0,0 +1,15 @@
+package back.vybz.auth_busker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@SpringBootApplication
+public class AuthBuskerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthBuskerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/back/vybz/auth_busker/AuthBuskerApplicationTests.java
@@ -0,0 +1,13 @@
+package back.vybz.auth_busker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthBuskerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
